f3903509076f1f3b5bb514bc5ad0a4bc0ba66c14
NaimGI <naimbenrhouma55@gmail.com>
2023-06-25 19:41:39 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+const Home=()=>{
+    return (<section className="flexStart flex-col paddings">
+        <h2>hello</h2>
+        <h2>hello</h2>
+        <h2>hello</h2>
+    </section>)
+}
+export default Home;
